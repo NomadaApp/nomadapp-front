0389f0cfbed6062d4e9ff255a34c0bbb3cb826a1
Manuel Castillo <manucalop@gmail.com>
2022-03-01 13:27:47 +0100

    Fixed app name

--- tests/test_greetings.py
+++ tests/test_greetings.py
@@ -1,11 +1,12 @@
 import unittest
-from nomadapp-front import greetings as grt
+from nomadapp_front import greetings as grt
+
 
 class TestSpreadsheetApi(unittest.TestCase):
     def test_hello(self):
         grt.hello()
-        self.assertEqual(True, True, 'I leave Python!')
+        self.assertEqual(True, True, "I leave Python!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
-
