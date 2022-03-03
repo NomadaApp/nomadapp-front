ce50528df3a0db0314da18898fb0e25e72878594
Antonio <ajimenezadalia@gmail.com>
2022-03-03 09:47:45 +0100

    redefining get data from api

--- nomadapp_front/get_data_from_api.py
+++ nomadapp_front/get_data_from_api.py
@@ -2,10 +2,21 @@
 
 import requests
 
+filters_dict = {
+        'location': 'Puerta del Sol, 1 Madrid',
+        'leisure': True,
+        'restaurants': True,
+        'education': True,
+        'coworking': True,
+        'radius': 5}
+
 
 # DATA FROM GOOGLE API
-def gcp_request_get(filters_dict):
+def gcp_request_get(query):
     url = "https://nomadapp-back-akukb5qdcq-ew.a.run.app/json-request"
-    response = requests.get(url, params=filters_dict)
+    response = requests.get(url, params=query)
     return response.json()
+
+
+
 
